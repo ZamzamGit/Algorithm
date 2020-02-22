19536a163797680f3c8a8345325672bb8fc04295
Khaled <s195487@student.dtu.dk>
2020-02-22 22:58:52 +0100

    Merge branch 'master' of /Users/khaledzamzam/IdeaProjects/Algorithm with conflicts.

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,14 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[] A = {1, 2, 1, 2, 4, 2, 2, 1,1,1,1,1,1,2,2,1};
+        int[] A = {3, 2, 2, 2, 2, 5, 5, 2};
         int[] B = {9, 9, 9, 9, 9, 9, 9, 9};
 
         System.out.println(ALG1(A));
         System.out.println(ALG3(A));
+        System.out.println(ALG1(B));
+        System.out.println(ALG3(B));
+
     }
 
     private static int ALG1(int[] A) {
@@ -34,33 +37,17 @@ public class Main {
     }
 
     private static int ALG3(int[] A) {
-        int combo = 1;
-        int highest = 0;
-
-        for (int i = 0; i < A.length-1 ; i++) {
-            if (A[i] == A[i+1]){
-                combo+=1;
-
-            }else if(combo > highest){
-
-                highest = combo;
-                     combo = 1;
-
-            } else {
-                combo = 1;
-
+        int l = 0;
+        int i = -1;
+        while (i < A.length) {
+            i++;
+            for (int j = i; j < A.length; j++) {
+                if (ALG2(A, i, j) && (j - i + 1) > l) {
+                    l = j - i + 1;
+                }
             }
-            if (i == A.length-2){
-                if (combo > highest) {
-                    highest = combo;
-                }
-                }
-
-
-
-
         }
-        return highest;
+        return l;
     }
 }
 
