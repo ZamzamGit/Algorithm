8407a5f274b30911ccc1ee99fef03012cf935f81
Sid-Ali-Mounib <sidali-m@hotmail.com>
2020-03-18 19:07:13 +0100

    sad

--- src/LightChain.java
+++ src/LightChain.java
@@ -4,9 +4,11 @@ import java.util.Scanner;
 public class LightChain {
 
     public static void main(String[] args) {
-
+        int c = 1;
+        int h = 0;
 
         Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
         String[] strArr = (scanner.nextLine().split(" "));
         int[] intArr = new int[strArr.length];
 
@@ -15,6 +17,29 @@ public class LightChain {
             intArr[i] = Integer.parseInt(num);
         }
         Arrays.sort(intArr);
-        System.out.print(Arrays.toString(intArr));
+
+
+
+        for (int i = 0; i < intArr.length-1 ; i++) {
+
+            if (intArr[i] == intArr[i+1]-1){
+                c+=1;
+
+            }else if(c > h){
+
+                h = c;
+                c= 1;
+
+            } else {
+                c = 1;
+
+            }
+            if (i == intArr.length-2){
+                if (c> h) {
+                    h = c;
+                }
+            }
+        }
+        System.out.println(h);
     }
 }
