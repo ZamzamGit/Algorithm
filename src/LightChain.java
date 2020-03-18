1fab4b19b41b8c3bd9144446944f7857f4d2ddf0
Sid-Ali-Mounib <sidali-m@hotmail.com>
2020-03-18 18:23:50 +0100

    sad

---
+++ src/LightChain.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class LightChain {
+
+
+    public static void main(String[] args) {
+
+
+        Scanner scanner = new Scanner(System.in);
+        String[] strArr = (scanner.nextLine().split(" "));
+        int[] intArr = new int[strArr.length];
+
+        for (int i = 0; i < strArr.length; i++) {
+            String num = strArr[i];
+            intArr[i] = Integer.parseInt(num);
+        }
+
+
+    }
+
+
+
+}
